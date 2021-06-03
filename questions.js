8f89d02dd13b4542a874c7bf214ea657b43a77ce
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-03 18:28:35 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I used to live in Berlin. I lived there ..... five years.",
+        options:["for", "during", "while"],
+        answer: 0
+    },
+    {
+        q:"One of the runners fell ..... the race but managed to get up and continue.",
+        options:["while", "during", "for"],
+        answer: 1
+    },
+    {
+        q:"Nobody came to see me ..... I was in hospital.",
+        options:["for", "during", "while"],
+        answer: 2
+    },
+    {
+        q:"Try to avoid travelling ..... the busy periods of the day.",
+        options:["during", "while", "for" ],
+        answer: 0
+    },
+    {
+        q:"I was very tired. I slept ..... ten hours.",
+        options:["during", "for", "while"],
+        answer: 1
+    },
+    {
+        q:"Can you hold my bag ..... I try on this jacket?",
+        options:["for", "during", "while"],
+        answer: 2
+    },
+    {
+     q:"I’m not sure when we’ll arrive, but it will be sometime ..... the afternoon.",
+        options:["during", "for", "while"],
+        answer: 0   
+    },
+    {
+        q:"I wasn’t well last week. I hardly ate anything ..... three days.",
+        options:["while", "for", "during"],
+        answer: 1
+    },
+    {
+        q:"My phone rang ..... we were having dinner.",
+        options:["for", "during", "while"],
+        answer: 2
+    },
+    {
+        q:"Nobody knows how many people were killed ..... the war.",
+        options:["during", "while", "for"],
+        answer: 0
+    }
+
+]
